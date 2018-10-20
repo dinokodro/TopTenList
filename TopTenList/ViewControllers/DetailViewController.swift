d6fd1eb36e4490cf0bf2c2805cae5a340324193f
dino.kodro <43411360+dinokodro@users.noreply.github.com>
2018-10-20 15:38:09 +0200

    UI changes

---
+++ TopTenList/ViewControllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  TopTenList
+//
+//  Created by Sabahudin Kodro on 20/10/2018.
+//  Copyright © 2018 Kodro. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TopTenList/ViewControllers/TopTenViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TopTenViewController.swift
+//  TopTenList
+//
+//  Created by Sabahudin Kodro on 20/10/2018.
+//  Copyright © 2018 Kodro. All rights reserved.
+//
+
+import UIKit
+
+class TopTenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        createNavBar()
+    }
+    
+    func createNavBar () {
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        let searchBar = searchController.searchBar
+        searchBar.showsCancelButton = false
+        searchBar.placeholder = "Search"
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.title = "Top Ten App"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
